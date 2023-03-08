3521f2d252b5cf26bd4a084bd51f9c8a896064cf
PeterOlayemi <olayemipeter2005@gmail.com>
2023-03-08 18:34:19 +0100

    another new commit

--- account/admin.py
+++ account/admin.py
@@ -1,16 +1,7 @@
 from django.contrib import admin
-from .models import Comment, Update, Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8, Image9
+from .models import Comment, Update
 
 # Register your models here.
 
 admin.site.register(Comment)
 admin.site.register(Update)
-admin.site.register(Image1)
-admin.site.register(Image2)
-admin.site.register(Image3)
-admin.site.register(Image4)
-admin.site.register(Image5)
-admin.site.register(Image6)
-admin.site.register(Image7)
-admin.site.register(Image8)
-admin.site.register(Image9)
--- account/views.py
+++ account/views.py
@@ -6,15 +6,6 @@ from .forms import CommentForm, UpdateForm
 # Create your views here.
 
 def index(request):
-    image1=Image1.objects.all()
-    image2=Image2.objects.all()
-    image3=Image3.objects.all()
-    image4=Image4.objects.all()
-    image5=Image5.objects.all()
-    image6=Image6.objects.all()
-    image7=Image7.objects.all()
-    image8=Image8.objects.all()
-    image9=Image9.objects.all()
     comment=Comment.objects.order_by('-date')
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -33,16 +24,6 @@ def index(request):
         cform = UpdateForm()
 
     context={
-        'image1':image1,
-        'image2':image2,
-        'image3':image3,
-        'image3':image3,
-        'image4':image4,
-        'image5':image5,
-        'image6':image6,
-        'image7':image7,
-        'image8':image8,
-        'image9':image9,
         'comment':comment,
         'form':form,
         'cform':cform
@@ -50,5 +31,4 @@ def index(request):
     return render(request, 'index.html', context)
 
 def SuccessView(request):
-    image3 = Image3.objects.all()
-    return render(request, 'success.html', {'image3':image3})
+    return render(request, 'success.html')
